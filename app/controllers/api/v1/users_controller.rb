d611162cfa8a2ff6dded38c9d98922bac6f8d36f
Daniel Macharia <danielhuios@gmail.com>
2022-12-22 00:53:15 +0300

    created signup method in user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token({user_id: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
+
+    def render_unprocessable_entity
+        render json: {error: "failed to create user"}, status: :unprocessable_entity
+    end
 end
