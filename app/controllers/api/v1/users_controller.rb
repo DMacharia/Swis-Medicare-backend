a28ee25d913fcec44099ae9490d0a65419a8a083
Daniel Macharia <danielhuios@gmail.com>
2022-12-22 01:15:01 +0300

    edited error handling on create method in user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::UsersController < ApplicationController
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
     def create
         @user = User.create(user_params)
         if @user.valid?
             @token = encode_token({user_id: @user.id})
             render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
-        end
+        else
+            render json: { error: 'failed to create user' }, status: :unprocessable_entity
+          end
     end
 
     private
@@ -14,7 +16,7 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
         params.permit(:username, :password)
     end
 
-    def render_unprocessable_entity
-        render json: {error: "failed to create user"}, status: :unprocessable_entity
-    end
+    # def render_unprocessable_entity
+    #     render json: {error: "failed to create user"}, status: :unprocessable_entity
+    # end
 end
