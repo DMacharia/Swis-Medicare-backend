d1db8edfdb21e33c91b069b0f19fa89243404bdd
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-22 16:01:33 +0300

    add the create method for the login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::AuthController < ApplicationController
+    def create
+        @user = User.find_by(username: login_params[:username])
+        if @user && @user.authenticate(login_params[:password])
+            @token = encode_token( {user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt:@token}, status: :accepted 
+        else
+            render json: {error: 'Invalid username or password' }, status: :unauthorized
+    end
+    
 end
