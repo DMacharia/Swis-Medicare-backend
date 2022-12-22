b7a69743309f4e979c4dac8b8b95c41845f96a48
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-22 16:27:10 +0300

    add the get profile route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         namespace :v1 do
             resources :users, only: [:index, :create]
             post '/login', to: 'auth#create'
+            get '/profile', to: 'users#profile'
         end
     end
 
