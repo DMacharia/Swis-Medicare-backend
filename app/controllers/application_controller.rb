74567f191bc7c81680bc322adedc3e4915ebb3f6
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-12-22 16:13:10 +0300

    add method to authenticate a current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+  
     def encode_token(payload)
         JWT.encode(payload, 'my_s3cr3t')
     end
@@ -16,5 +18,20 @@ class ApplicationController < ActionController::API
             nil
           end
         end
+      end
+
+      def current_user
+        if decoded_token
+          user_id = decoded_token[0]['user_id']
+          @user = User.find_by(id: user_id)
+        end
+      end
+    
+      def logged_in?
+        !!current_user
+      end
+    
+      def authorized
+        render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
       end
 end
