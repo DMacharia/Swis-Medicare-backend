4565588dfce68de0ea241493d412e5eef8297a59
Daniel Macharia <danielhuios@gmail.com>
2023-01-05 17:29:01 +0300

    modified the users table

--- db/migrate/20221221213450_create_users.rb
+++ db/migrate/20221221213450_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :second_name
       t.string :username
+      t.string :email
+      t.string :reg_no
+      t.integer :mobile_no
+      t.integer :emergency_no
+      t.string :address
+      t.string :gender
+      t.string :blood_group
       t.string :password_digest
       t.string :title
       t.string :speciality
