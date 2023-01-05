0b9ba68e5065eac45063e8b27188f4ab7ac863c5
Daniel Macharia <danielhuios@gmail.com>
2023-01-05 12:36:59 +0300

    edited medical history controller & did migration

--- app/controllers/medical_historys_controller.rb
+++ app/controllers/medical_historys_controller.rb
@@ -1,5 +1,6 @@
 class MedicalHistorysController < ApplicationController
    skip_before_action :authorized, only: [:create, :index]
+   
     def index
         if params[:patient_id]
             @patient = Patient.find(params[:patient_id])
