cd10ecb7d4340ff30d84f56a79d7afacde441ac1
Daniel Macharia <danielhuios@gmail.com>
2023-01-05 18:36:02 +0300

    modified medical history model, serializer, controller

--- app/serializers/medical_history_serializer.rb
+++ app/serializers/medical_history_serializer.rb
@@ -1,3 +1,3 @@
 class MedicalHistorySerializer < ActiveModel::Serializer
-  attributes :id, :patient_id, :condition, :treatment, :notes
+  attributes :id, :patient_id, :weight, :blood_pressure, :blood_sugar, :temperature, :pulse_rate, :test, :diagnosis, :medication, :notes
 end
