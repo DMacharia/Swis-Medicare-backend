fb9b87c29726172034de70696f12d431ccc554ca
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-07 13:20:48 +0300

    add attributes to the users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :role
+  attributes :id, :first_name, :second_name, :username,:email, :mobile_no, :address, :password, :role
 end
