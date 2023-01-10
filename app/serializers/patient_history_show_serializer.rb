f4dd4829ef9bde3bdcf088198f9aeb9fee5fe112
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-10 17:28:58 +0300

    create a showpatienthistory custom serializer

---
+++ app/serializers/patient_history_show_serializer.rb
@@ -0,0 +1,4 @@
+class PatientHistoryShowSerializer < ActiveModel::Serializer
+  attributes :id, :first_name, :second_name, :username, :email, :mobile_no, :gender, :address, :blood_group, :password, :date_of_birth, :role
+  has_many :patient_histories
+end
