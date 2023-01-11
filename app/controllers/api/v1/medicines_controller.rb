b78961b2bb8cd9d25f32e4013e71748725d2100c
Daniel Macharia <danielhuios@gmail.com>
2023-01-11 21:52:03 +0300

    added full CRUD actions on the medicine controller

--- app/controllers/api/v1/medicines_controller.rb
+++ app/controllers/api/v1/medicines_controller.rb
@@ -3,4 +3,46 @@ class Api::V1::MedicinesController < ApplicationController
         @medicines = Medicine.all
         render json: @medicines
     end
+
+    def create
+        @medicine = medicine.create(medicine_params)
+        render json: { medicine: MedicineSerializer.new(@medicine) }, status: :created
+        else
+            render json: { error: 'failed to create medicine' }, status: :unprocessable_entity
+        end
+    end
+
+
+    def show
+        @medicine = find_medicine
+        render json: @medicine, serializer: MedicineSerializer, status: :ok
+    end
+
+
+    def update
+        @medicine = find_medicine
+            @medicine.update(medicine_params)
+            render json: @medicine, status: :ok
+    end
+
+    #DELETE /@medicines/:id
+    def destroy
+        #find & destroy
+        @medicine = find_medicine
+            @medicine.destroy
+            head :no_content
+    end
+
+    def medicine_params
+        params.permit(:name, :dosage, :manufacturer, :quantity, :expiry_date, :price)
+     end
+ 
+     def render_not_found
+         render json: {message: "medicine not found" }, status: :not_found
+     end
+ 
+     def find_medicine
+         Medicine.find(params[:id])
+     end
+
 end
