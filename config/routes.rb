7dc24a69e0bacb953f2f62b17f83a266c4312b8e
Daniel Macharia <danielhuios@gmail.com>
2023-01-11 18:56:05 +0300

    edited routes to accomodate only index action on users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
     namespace :api do
         namespace :v1 do
-            resources :users, only: [:index, :create]
+            resources :users, only: [:index]
             resources :admins
             resources :doctors
             resources :patients
