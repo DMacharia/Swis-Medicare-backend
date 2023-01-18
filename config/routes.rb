b93c17ece99ddaac071652bc9eae1ed18e05cb3f
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-18 23:31:05 +0300

    add changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :mpesas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -20,4 +21,7 @@ Rails.application.routes.draw do
 
         end
     end
+    resources :mpesas
+    post 'stkpush', to: 'mpesas#stkpush'
+    post 'stkquery', to: 'mpesas#stkquery'
 end
