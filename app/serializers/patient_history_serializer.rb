bafc25b589418c556dcaf4ffdc06e83dbd1c2286
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2023-01-19 00:59:13 +0300

    modify serializer

--- app/serializers/patient_history_serializer.rb
+++ app/serializers/patient_history_serializer.rb
@@ -1,3 +1,4 @@
 class PatientHistorySerializer < ActiveModel::Serializer
     attributes :id, :patient_id, :weight, :blood_pressure, :blood_sugar, :temperature, :pulse_rate, :test, :diagnosis, :medication, :notes
+    has_many :patient_histories
 end
